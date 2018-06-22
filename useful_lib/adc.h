743f031e445bd361db5af5f77d9a22672fc99c23
Jose Guerra Carmenate <joseguerracarmenate@gmail.com>
2018-06-22 00:25:16 -0400

    Comments fixeds

--- useful_lib/adc.h
+++ useful_lib/adc.h
@@ -29,22 +29,22 @@ extern "C" {
 /* CONSTANTS */
 
 const uint8_t 
-        AN0  = 0x00, /* Analog Channel 0 --> CEREBOT JJ-01  */
-        AN1  = 0x01, /* Analog Channel 1 --> CEREBOT JJ-02*/
-        AN2  = 0x02, /* Analog Channel 2 --> CEREBOT JJ-03 */
-        AN3  = 0x03, /* Analog Channel 3 --> CEREBOT JJ-04 */
-        AN4  = 0x04, /* Analog Channel 4 --> CEREBOT JJ-07 */
-        AN5  = 0x05, /* Analog Channel 5 --> CEREBOT JJ-08 */
-        AN6  = 0x06, /* Analog Channel 6 --> CEREBOT Debug Circuit */
-        AN7  = 0x07, /* Analog Channel 7 --> CEREBOT Debug Circuit */
-        AN8  = 0x08, /* Analog Channel 8 --> CEREBOT JJ-09 */
-        AN9  = 0x09, /* Analog Channel 9 --> CEREBOT JJ-10 */
+        AN0  = 0x00, /* Analog Channel 0 --> CEREBOT JJ-01                   */
+        AN1  = 0x01, /* Analog Channel 1 --> CEREBOT JJ-02                   */
+        AN2  = 0x02, /* Analog Channel 2 --> CEREBOT JJ-03                   */
+        AN3  = 0x03, /* Analog Channel 3 --> CEREBOT JJ-04                   */
+        AN4  = 0x04, /* Analog Channel 4 --> CEREBOT JJ-07                   */
+        AN5  = 0x05, /* Analog Channel 5 --> CEREBOT JJ-08                   */
+        AN6  = 0x06, /* Analog Channel 6 --> CEREBOT Debug Circuit           */
+        AN7  = 0x07, /* Analog Channel 7 --> CEREBOT Debug Circuit           */
+        AN8  = 0x08, /* Analog Channel 8 --> CEREBOT JJ-09                   */
+        AN9  = 0x09, /* Analog Channel 9 --> CEREBOT JJ-10                   */
         AN10 = 0x0A, /* Analog Channel 10 --> CEREBOT JK-01, Shared with LD1 */
         AN11 = 0x0B, /* Analog Channel 11 --> CEREBOT JK-02, Shared with LD2 */
         AN12 = 0x0C, /* Analog Channel 12 --> CEREBOT JK-03, Shared with LD3 */
         AN13 = 0x0D, /* Analog Channel 13 --> CEREBOT JK-04, Shared with LD4 */
-        AN14 = 0x0E, /* Analog Channel 14 --> CEREBOT JB-10 */
-        AN15 = 0x0F; /* Analog Channel 15 --> CEREBOT JB-07 */
+        AN14 = 0x0E, /* Analog Channel 14 --> CEREBOT JB-10                  */
+        AN15 = 0x0F; /* Analog Channel 15 --> CEREBOT JB-07                  */
     
     
 /* FUNCTIONS */    
@@ -52,18 +52,19 @@ const uint8_t
 /* ------------------------------------------------------------ */
 /*        void ADC_SetUp( uint32_t desiredAdcClock )
 **
-**        Synopsis:
+**        @Synopsis:
 **          ADC_SetUp( ClockForAdcModule );
 ** 
-**        Parameters:
+**        @Parameters:
 **          uint32_t desiredAdcClock - Desired clock frequency for ADC module
 **
-**        Return Values:
+**        @Return Values:
 **          None
 **
-**        Errors:
+**        @Errors:
 **          None
-**        Description:
+**        
+**        @Description:
 **          The function implements the configuration sequence for ADC 
 **         module. This function only need be called one time.
 **				
@@ -74,22 +75,23 @@ void ADC_SetUp( uint32_t desiredAdcClock  );
 /* ------------------------------------------------------------ */
 /*        void ADC_SetUpDefault( )
 **
-**        Synopsis:
+**        @Synopsis:
 **          ADC_SetUp( );
 ** 
-**        Parameters:
+**        @Parameters:
 **          None
 **
-**        Return Values:
+**        @Return Values:
 **          None
 **
-**        Errors:
+**        @Errors:
 **          None
-**        Description:
+**        
+**        @Description:
 **          The function execute ADC_SetUp function with a default value
 **         for ADC frecuence.
 **				
-*/
+ */
 inline void ADC_SetUpDefault(  ){
     ADC_SetUp( _ADC_DEFAULT_FREQ );
 }
@@ -98,23 +100,23 @@ inline void ADC_SetUpDefault(  ){
 /* ------------------------------------------------------------ */
 /*        uint16_t ADC_Read( uint8_t analogChannel)
 **
-**        Synopsis 
+**        @Synopsis 
 **          ADC_Read( aChannelToRead);
 ** 
-**        Parameters
+**        @Parameters
 **          uint8_t aChannelToRead - the analog channel to read
 **
-**        Return Values
+**        @Return Values
 **          uint16_t - the value that was read => [0; 1023]
 **
-**        Errors:
+**        @Errors:
 **          None
 ** 
-**        Preconditions:
+**        @Preconditions:
 **          ADC_SetUp or ADC_SetUpDefault function need be called before
 **         call ADC_Read
 ** 
-**        Description:
+**        @Description:
 **          The function implements a ADC read sequence. 
 **				
 */
@@ -123,7 +125,7 @@ uint16_t ADC_Read( uint8_t analogChannel);
 
 /*        uint16_t analogRead( uint8_t analogChannel )
  *        
- *        Note:
+ *        @Note:
  *          Just like ADC_Read function. See ADC_Read function for reference
  * 
  */
