cbd4066e48f139bd40ddae4406b7662ea6042c2c
Jose Guerra Carmenate <joseguerracarmenate@gmail.com>
2018-06-27 02:26:11 -0400

    Sensor library created

---
+++ Sensor.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Sensor.X.a
+CND_ARTIFACT_PATH_default=dist/default/production/Sensor.X.a
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=Sensor.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/Sensor.X.tar
